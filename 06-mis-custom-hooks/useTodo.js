143caae6586294f7da48c41210c1b7791912467d
StivenFon2u <110936940+stivenfon97@users.noreply.github.com>
2024-10-25 11:47:03 -0500

    mi primer commit

---
+++ 06-mis-custom-hooks/useTodo.js
@@ -0,0 +1,63 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+const initialState = [
+  // {
+  //   id: new Date().getTime(),
+  //   description: "Recolectar la piedra del Alma",
+  //   done: false,
+  // },
+  // {
+  //   id: new Date().getTime() * 3,
+  //   description: "Recolectar la piedra del Tiempo",
+  //   done: false,
+  // },
+];
+
+export const useTodo = () => {
+
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos) || []);
+  }, [todos]);
+
+  const handleNewTodo = (todo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: todo,
+    };
+
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: "[TODO] Delete Todo",
+      payload: id,
+    });
+  };
+
+  const hadleToggleTodo = (id) => {
+    dispatch({
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    });
+  };
+
+  const countTodos = todos.length;
+  const pendingTodosCount = todos.filter(todo => !todo.done).length;
+
+  return {
+    todos,
+    countTodos,
+    pendingTodosCount,
+    handleNewTodo,
+    handleDeleteTodo,
+    hadleToggleTodo,
+  };
+};
